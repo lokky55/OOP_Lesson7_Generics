13696d9dded33f6b6c2690dc6960f71f787140a3
lokky55 <lokky55@mail.ru>
2022-06-21 22:49:15 +0300

    fixed

--- src/main/java/ru/netology/statistic/Main.java
+++ src/main/java/ru/netology/statistic/Main.java
@@ -5,10 +5,6 @@ import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
-//        Book book1 = new Book("Garry", "Rouling", 2000, 230);
-//        Book book2 = new Book("Anton", "Rouling", 1900, 330);
-//        int result = book1.compareTo(book2);
-//        System.out.println(result);
 
         Book[] books = {
         new Book("Garry", "Rouling", 2000, 230),
@@ -16,9 +12,8 @@ public class Main {
         new Book("Winter", "Alex", 2001, 130),
         new Book("Oscar", "unknown", 1999, 430),
         };
-        // Красивый вывод массивов, при этом задается параметр toString в дата-классе
 
         Arrays.sort(books);
-        System.out.println(Arrays.toString(books));
+        System.out.println(Arrays.toString(books)); // Красивый вывод массивов, при этом задается параметр toString в дата-классе
     }
 }
--- src/test/java/ru/netology/statistic/LibraryTest.java
+++ src/test/java/ru/netology/statistic/LibraryTest.java
@@ -6,22 +6,18 @@ import org.junit.jupiter.api.Test;
 
 public class LibraryTest {
 
-    Library library = new Library();
     Book book = new Book("Garry", "Rouling", 2000, 230);
     Book book1 = new Book("Anton", "Gousling", 1900, 330);
     Book book2 = new Book("Winter", "Alex", 2001, 130);
     Book book3 = new Book("Oscar", "unknown", 1999, 430);
 
-    @BeforeEach
-    public void setUp() {
+    @Test
+    public void shouldFindAndSortByYear() {
+        Library library = new Library();
         library.addBook(book);
         library.addBook(book1);
         library.addBook(book2);
         library.addBook(book3);
-    }
-
-    @Test
-    public void shouldFindAndSortByYear() {
         Book[] actual = library.findBooksByYear(1900, 2000);
         Book[] expected = {book1, book3, book};
         Assertions.assertArrayEquals(expected, actual);
